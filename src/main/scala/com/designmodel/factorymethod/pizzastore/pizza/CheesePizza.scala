c3a24cd632e28ea71ff00c88db3523f2bdecf7fa
spencer_spark <spencer_spark@sina.com>
2019-09-23 16:42:48 +0800

    learn scala

---
+++ src/main/scala/com/designmodel/factorymethod/pizzastore/pizza/CheesePizza.scala
@@ -0,0 +1,15 @@
+package com.designmodel.factorymethod.pizzastore.pizza
+
+/**
+  * @ Author     ：zsp
+  * @ Date       ：Created in 15:28 2019/9/23
+  * @ Description：
+  * @ Modified By：
+  * @ Version:     
+  */
+class CheesePizza extends Pizza {
+  override def prepare(): Unit = {
+    this.name = "奶酪pizza"
+    println(this.name + " preparing")
+  }
+}
---
+++ src/main/scala/com/designmodel/factorymethod/pizzastore/pizza/GreekPizza.scala
@@ -0,0 +1,15 @@
+package com.designmodel.factorymethod.pizzastore.pizza
+
+/**
+  * @ Author     ：zsp
+  * @ Date       ：Created in 15:09 2019/9/23
+  * @ Description：
+  * @ Modified By：
+  * @ Version:     
+  */
+class GreekPizza extends Pizza {
+  override def prepare(): Unit = {
+    this.name = "希腊pizza"
+    println(this.name + "preparing")
+  }
+}
